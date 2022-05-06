64b1bc63c159d0ced129ec0c6419cd4b655d8755
AmineKamrobotica <amine.kammoun@robotika.systems>
2022-05-06 12:42:29 +0100

    2nd commit

--- socket.js
+++ socket.js
@@ -1,13 +1,38 @@
+console.log("welcome to ansible manipulation");
 const { exec } = require("child_process");
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-exec("cd systemdmanager && ansible-playbook apache.yml", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("openApache", (msg) => {
+    console.log("openning apache ...");
+    exec(
+      "cd systemdmanager && ansible-playbook rabitmq.yml",
+      (error, stdout, stderr) => {
+        if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+        }
+        if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+      }
+    );
+  });
+});
+
+/*  */
+server.listen(process.env.PORT || 3000, () => {
+  console.log("listening on *:3000");
 });
